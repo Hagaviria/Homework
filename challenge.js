f1bcf2ddbd2ff28f8708b9cf69234821eb2d34ae
Harold Gaviria Londoño <hagaviria@hotmail.com>
2023-08-02 22:54:50 -0500

    challenge

---
+++ challenge.js
@@ -0,0 +1,22 @@
+const Parity = function (y){
+    
+    if ( y % 2 === 0){
+        console.log ("El numero es par");
+    }else{
+        console.log ("el numero es impar");
+    }
+    return y
+}
+console.log (Parity(4))
+
+const Function= (y) => {
+
+    if ( x % 2 === 0){
+        console.log ("El numero es par");
+    }else{
+        console.log ("el numero es impar");
+    }
+    return y
+
+} 
+console.log (Function(9))
