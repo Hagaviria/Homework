70d7d25294c9f88f0a84e678e1ce7b409fc500a0
Hagaviria <hagaviria@hotmail.com>
2023-10-11 21:20:14 -0500

    challenge 9

--- challenge6/src/main.jsx
+++ challenge6/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './FirstApp.css'
-import {FirstApp}from './Components/FirstApp';
+import{HookC} from './Challenge9/HookC'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
- <FirstApp/>
+ <HookC/>
   </React.StrictMode>,
 )
