8099fdd16417a436abb6457509fcbdeff907674c
Hagaviria <hagaviria@hotmail.com>
2023-10-11 19:07:18 -0500

    hallenge7

--- challenge6/src/main.jsx
+++ challenge6/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Handle } from './Components/Handle.jsx'
+import { Categories } from './Components/Categories'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
- <Handle value = {1}/>
+ <Categories/>
   </React.StrictMode>,
 )
