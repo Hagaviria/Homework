33470b7942ba4c4c13f8e369db193a1080990ecb
Hagaviria <hagaviria@hotmail.com>
2023-10-11 21:37:55 -0500

    challenge fetch

--- challenge6/src/main.jsx
+++ challenge6/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './FirstApp.css'
 import{HookC} from './Challenge9/HookC'
+import { Giphy } from './Challenge10/Giphy'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
- <HookC/>
+  <Giphy></Giphy>
   </React.StrictMode>,
 )
