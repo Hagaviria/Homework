18ed7418d84ad506b68d310560d2d4a430946a1c
Hagaviria <hagaviria@hotmail.com>
2023-10-11 22:25:07 -0500

    challenge todo

--- challenge6/src/main.jsx
+++ challenge6/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './Challenge 12/Father.css'
+import './Challenge 13/TodoEst.css'
 import { MultipleCustomHooks } from './Challenge 11/MultipleCustomHooks'
 import { Father } from './Challenge 12/Father'
+import { TodoApp } from './Challenge 13/TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <Father/>
+     <TodoApp/>
   </React.StrictMode>,
 )
